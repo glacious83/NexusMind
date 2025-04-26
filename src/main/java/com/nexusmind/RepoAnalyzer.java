02206c4ee777dc8e85a5dd912dc364177d4bc526
mmamouze <mmamouze@netcompany.com>
2025-04-26 10:44:15 +0300

    Optimize RepoAnalyzer with depth limit and folder summarization for large projects

--- src/main/java/com/nexusmind/RepoAnalyzer.java
+++ src/main/java/com/nexusmind/RepoAnalyzer.java
@@ -7,9 +7,13 @@ import java.util.List;
 public class RepoAnalyzer {
 
     private final String repoPath;
+    private final int maxDepth;
+    private final int folderSummarizationThreshold;
 
     public RepoAnalyzer(String repoPath) {
         this.repoPath = repoPath;
+        this.maxDepth = 3; // Limit depth to avoid scanning too deeply
+        this.folderSummarizationThreshold = 10; // If folder has more than 10 Java files, summarize
     }
 
     public String generateProjectSummary() {
@@ -20,7 +24,7 @@ public class RepoAnalyzer {
     }
 
     private void scanDirectory(File directory, int depth, List<String> summaryLines) {
-        if (!directory.isDirectory()) {
+        if (!directory.isDirectory() || depth > maxDepth) {
             return;
         }
 
@@ -29,14 +33,30 @@ public class RepoAnalyzer {
             return;
         }
 
+        int javaFileCount = 0;
+        List<File> subDirectories = new ArrayList<>();
+
         for (File file : files) {
-            String prefix = "  ".repeat(depth);
             if (file.isDirectory()) {
-                summaryLines.add(prefix + "[Folder] " + file.getName());
-                scanDirectory(file, depth + 1, summaryLines);
+                subDirectories.add(file);
             } else if (file.getName().endsWith(".java")) {
-                summaryLines.add(prefix + "[File] " + file.getName());
+                javaFileCount++;
             }
+        }
+
+        if (javaFileCount > folderSummarizationThreshold) {
+            summaryLines.add("  ".repeat(depth) + "[Folder] " + directory.getName() + " (Contains " + javaFileCount + " Java files)");
+        } else {
+            for (File file : files) {
+                if (file.isFile() && file.getName().endsWith(".java")) {
+                    summaryLines.add("  ".repeat(depth) + "[File] " + file.getName());
+                }
+            }
+        }
+
+        for (File subDir : subDirectories) {
+            summaryLines.add("  ".repeat(depth) + "[Folder] " + subDir.getName());
+            scanDirectory(subDir, depth + 1, summaryLines);
         }
     }
 }
